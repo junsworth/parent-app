1def71709b6da63eadf228d2c89b6832ed5bf628
Jonathan Unsworth <jonathan@bubbleworks.co.za>
2017-02-12 15:21:47 +0200

    Initial commit...

---
+++ ParentApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParentApp
+//
+//  Created by Jonathan Unsworth on 2017/02/12.
+//  Copyright © 2017 Jonathan Unsworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ParentAppTests/ParentAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  ParentAppTests.swift
+//  ParentAppTests
+//
+//  Created by Jonathan Unsworth on 2017/02/12.
+//  Copyright © 2017 Jonathan Unsworth. All rights reserved.
+//
+
+import XCTest
+@testable import ParentApp
+
+class ParentAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
